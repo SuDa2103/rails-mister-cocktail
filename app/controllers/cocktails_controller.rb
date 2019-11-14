defe35aafc47acdeceedee9c7260b2d056eeb3e3
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2019-11-14 14:31:18 +0000

    doses models and controller actions added with a new view

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,46 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show; end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @cocktail.update(cocktail_params)
+       redirect_to @cocktail
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
